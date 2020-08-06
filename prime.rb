4b78aa808a4b441db283515a5e4987cf58a517e5
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 06:59:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-def prime?()
+def prime?(integer)
+  if integer <= 1 || integer == 0 || integer == 1
+    return FALSE
+    elsif 
+    (2..integer - 1).each do |i|
+      if integer % i == 0 
+        return FALSE
+      end
+    end
+  end
+  TRUE
+end
