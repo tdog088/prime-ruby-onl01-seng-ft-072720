5cf1a82332854dbfbad01fef298f000453414ef5
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 07:02:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1 || integer == 0 || integer == 1
+    return FALSE
+    elsif 
+    (2..integer - 1).each do |i|
+      if integer % i == 0 
+        return FALSE
+      end
+    end
+  end
+  TRUE
+end
